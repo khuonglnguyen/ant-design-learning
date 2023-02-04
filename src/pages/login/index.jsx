cccc0486b2b80470c468b0ca76587abb30e11a60
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-02-04 19:37:16 +0700

    alert

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,14 +1,18 @@
 import "../../App.css";
-import { Button, Form, Input } from "antd";
+import { Alert, Button, Form, Input, message } from "antd";
+import { useState } from 'react'
 
 function Login() {
+    const [alert, setAlert] = useState(false)
     const onFinish = () => {
-        alert("Logged")
+        setAlert(true)
+        message.success("Logged")
     }
 
     return (
         <div className="App">
             <header className="App-header">
+                {alert && <Alert type="success" description="Logged"></Alert>}
                 <Form onFinish={onFinish}>
                     <Form.Item label="Username" name="username" required>
                         <Input placeholder="Username"></Input>
